88a91531e17818385f4fd86759d595f6a6aeeba6
gokul <gokulchandan24@gmail.com>
2024-10-14 19:22:49 +0530

    input added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCf52RVuAl4ycJ6a3oW5MCO3wZWMirLr20",
+  authDomain: "next-social-app-3d928.firebaseapp.com",
+  projectId: "next-social-app-3d928",
+  storageBucket: "next-social-app-3d928.appspot.com",
+  messagingSenderId: "9987709166",
+  appId: "1:9987709166:web:234a4218ea01aceca7fbff",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
